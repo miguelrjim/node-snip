ffdc5c2a103e994d126f551c3cd680a87aeadc18
Miguel Jimenez <miguelrjim@gmail.com>
2021-08-20 16:43:32 -0700

    IPv4 implementation

--- snip.js
+++ snip.js
@@ -1,27 +1,27 @@
-var sni = require('sni-reader');
-var net = require('net');
-var dns = require('dns');
-var log4js = require('log4js');
-var dnsLog = log4js.getLogger('dns');
-var tcpLog = log4js.getLogger('tcp');
-var sniLog = log4js.getLogger('sni');
-var AsyncCache = require('async-cache');
+const sni = require('sni-reader');
+const net = require('net');
+const dns = require('dns');
+const log4js = require('log4js');
+const dnsLog = log4js.getLogger('dns');
+const tcpLog = log4js.getLogger('tcp');
+const sniLog = log4js.getLogger('sni');
+const AsyncCache = require('async-cache');
 
-var port = process.env.PORT || 443;
+const port = process.env.PORT || 443;
 if (process.env.DNS) {
 	dns.setServers(process.env.DNS.split(','));
 }
-var shutdownGrace = process.env.SHUTDOWN_GRACE || 5000;
-dnsLog.setLevel(process.env.LOG_LEVEL || 'INFO');
-tcpLog.setLevel(process.env.LOG_LEVEL || 'INFO');
-sniLog.setLevel(process.env.LOG_LEVEL || 'INFO');
+const shutdownGrace = process.env.SHUTDOWN_GRACE || 5000;
+dnsLog.level = process.env.LOG_LEVEL || 'INFO';
+tcpLog.level = process.env.LOG_LEVEL || 'INFO';
+sniLog.level = process.env.LOG_LEVEL || 'INFO';
 
-var dnsCache = new AsyncCache({
+const dnsCache = new AsyncCache({
 	max: 1000,
 	maxAge: process.env.DNS_CACHE || 3600 * 1000,
 	load: function (key, cb) {
-		dnsLog.debug('Looking up AAAA', key);
-		dns.resolve6(key, cb);
+		dnsLog.debug('Looking up A', key);
+		dns.resolve4(key, cb);
 	}
 });
 
@@ -32,19 +32,19 @@ function initSession(serverSocket, sniName) {
 			dnsLog.warn(serverSocket.remoteAddress, sniName, 'resolve', err ? err.code : null);
 			return;
 		}
-		var ip = addresses[0];
-		var clientSocket = net.connect({port: 443, type: 'tcp6', host: ip});
+		const ip = addresses[0];
+		const clientSocket = net.connect({ port: 443, type: 'tcp', host: ip });
 		tcpLog.debug(serverSocket.remoteAddress, sniName, 'connecting', addresses);
 
-		clientSocket.on('connect', function() {
+		clientSocket.on('connect', function () {
 			serverSocket.pipe(clientSocket).pipe(serverSocket);
 			tcpLog.info(serverSocket.remoteAddress, sniName, 'connected', ip);
 		});
-		clientSocket.on('error', function(err) {
+		clientSocket.on('error', function (err) {
 			tcpLog.error(sniName, 'Client socket reported', err.code);
 			serverSocket.end();
 		})
-		serverSocket.on('error', function(err) {
+		serverSocket.on('error', function (err) {
 			tcpLog.error(serverSocket.remoteAddress, 'Server socket reported', err.code);
 			clientSocket.end();
 		})
@@ -56,7 +56,7 @@ function interrupt() {
 	server.getConnections(function (err, count) {
 		if (!err && count) {
 			console.error('Waiting for clients to disconnect. Grace', shutdownGrace);
-			setTimeout(function() {
+			setTimeout(function () {
 				process.exit();
 			}, shutdownGrace);
 		} else if (err) {
@@ -66,14 +66,14 @@ function interrupt() {
 	});
 };
 
-var server = net.createServer(function (serverSocket) {
-	sni(serverSocket, function(err, sniName) {
+const server = net.createServer(function (serverSocket) {
+	sni(serverSocket, function (err, sniName) {
 		if (err) {
 			sniLog.trace(err);
 			serverSocket.end();
 		} else if (sniName) {
 			sniLog.debug(serverSocket.remoteAddress, sniName);
-			serverSocket.on('error', function(err){
+			serverSocket.on('error', function (err) {
 				if (err.code == 'EPIPE') {
 					sniLog.debug(serverSocket.remoteAddress, 'Client disconnected before the pipe was connected.');
 				} else {
@@ -88,6 +88,6 @@ var server = net.createServer(function (serverSocket) {
 		}
 	});
 }).listen(port, '0.0.0.0');
-tcpLog.debug('Started listening on tcp4 port', port);
+tcpLog.info('Started listening on tcp4 port', port);
 
 process.once('SIGINT', interrupt);
